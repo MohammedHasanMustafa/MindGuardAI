b5f45de87c3f280558a9d3a8a707f63b02d8ab76
HASAN MUSTAFA <126100102+MohammedHasanMustafa@users.noreply.github.com>
2025-05-24 10:09:03 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://mindguardaibackend.onrender.com/api/auth/signup", { fullName, email, password });
+      await axios.post("http://localhost:5000/api/user/register", { fullName, email, password });
       alert("Signup successful! Please login.");
       navigate("/login");
     } catch (error) {
